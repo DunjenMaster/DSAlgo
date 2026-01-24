bf1fb691bf7b208851bb1ffdd67696b4787688b3
DunjenMaster <developmentwithutkarsh@gmail.com>
2026-01-24 22:43:06 +0530

    Arrays:Natual Sum

---
+++ GfG/Arrays/NaturalSum.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class NaturalSum {
+
+    static long naturalSum(long n) {
+
+        long sum = 0;
+
+        for (long i = 1; i <= n; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println(naturalSum(n));
+        sc.close();
+    }
+
+}
